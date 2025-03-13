abce73d0af3a876b2cb0e3333fe56593a9464d0c
Plinschi Alexandru <Alexandru Plinschi@DESKTOP-LTEF5V5>
2025-03-13 14:24:05 +0200

    Added CPU structure

--- 6502/6502.cpp
+++ 6502/6502.cpp
@@ -6,7 +6,20 @@ struct CPU {
 	using Word = unsigned short;
 
 //Program counter
-	Word PC
+	Word PC;
+	//Stack pointer
+	Word SP;
+
+	Byte A, Y, X; //Registers
+
+	//Status flags
+	Byte C : 1;
+	Byte Z : 1;
+	Byte I : 1;
+	Byte D : 1;
+	Byte B : 1;
+	Byte V : 1;
+	Byte N : 1;
 };
 
 int main() {
